ea678c35528a3fc915fa3208b58394f495a3ed0b
Muhammad Anas <muhammadannas138@gmail.com>
2025-11-20 19:42:30 +0500

    added extra structure

--- Backend/app.js
+++ Backend/app.js
@@ -5,9 +5,13 @@ import cors from "cors"
 import HomeRouter from "./routes";
 import authroutes from "./routes/authroutes";
 import authorization from "./middleware/auth";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(helmet);
+app.use(cookieParser());
+app.use(helmet());
 // app.use(cors())
 // app.use(limiter(
 
