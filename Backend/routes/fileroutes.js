0594c708731fac7714a86984416d2b99fc7189b8
Muhammad Anas <muhammadannas138@gmail.com>
2025-11-25 22:19:58 +0500

    added multer for receiving files

--- Backend/routes/fileroutes.js
+++ Backend/routes/fileroutes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { getFile,uploadFile } from "../controllers/files.js";
+import upload from "../middleware/multer.js";
 
 const filerouter = Router();
 
 filerouter.get("/files/:id",getFile)
-filerouter.post("/files/:parentid",uploadFile)
+filerouter.post("/files/:parentid",upload.single("file"),uploadFile)
 
 export default filerouter;
