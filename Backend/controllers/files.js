c116b1c30df9b63d45165d32133c08527ff7d093
Muhammad Anas <muhammadannas138@gmail.com>
2025-11-26 23:01:19 +0500

    backend file routes partially complete

--- Backend/controllers/files.js
+++ Backend/controllers/files.js
@@ -1,8 +1,23 @@
 import { queryDB } from "../db/index.js";
-async function getFile(){
-
+async function getFile(req,res){
+    const userid = req.user.userid
+    const id = req.params.id
+    try {
+        const result = queryDB("select * from items where id = $1 and userid = $2"[userid,id])
+        return res.status(200).json(result[0])
+    } catch (error) {
+        return res.status(400).json({msg: "failed to fetch file"})
+    }
 }
-async function uploadFile(){
-
+async function uploadFile(req,res){
+    const userid = req.user.userid
+    const parent = req.params.parentid
+    const file = req.file
+    try {
+        await queryDB("insert into items () values ()",[userid, parent, file])
+        return res.json({msg: "file uploaded successfully"})
+    } catch (error) {
+        return res.json({msg: "file upload failed"})
+    }
 }
 export {getFile,uploadFile}
