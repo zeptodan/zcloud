e05c60510f47712a6d872a1290ee3b63020d36a8
Muhammad Anas <muhammadannas138@gmail.com>
2025-12-11 20:28:14 +0500

    added docker build files

--- Backend/db/index.js
+++ Backend/db/index.js
@@ -2,7 +2,7 @@ import {Pool} from "pg"
 
 const pool = new Pool({
         user:process.env.USER,
-        host:"localhost",
+        host:process.env.HOST,
         database:process.env.DATABASE,
         password:process.env.PASSWORD,
         port:process.env.DB_PORT,
